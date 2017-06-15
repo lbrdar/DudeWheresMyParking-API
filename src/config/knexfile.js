b835722391ecf2575f1fb1816d113b05753de056
Leo Brdar <lbrdar@riteh.hr>
2017-06-15 23:39:56 +0200

    Add db for staging & production

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -13,12 +13,42 @@ module.exports = {
   },
 
 
-  staging: {
-   // TODO
+  staging: {  // FIXME: use different setup in staging
+    client: 'mysql',
+    connection: {
+      host: 'sql11.freemysqlhosting.net',
+      port: 3306,
+      user: 'sql11180430',
+      password: 'sihZ1l4k8B',
+      database: 'sql11180430',
+      dateStrings: true,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
 
   production: {
-    // TODO
+    client: 'mysql',
+    connection: {
+      host: 'sql11.freemysqlhosting.net',
+      port: 3306,
+      user: 'sql11180430',
+      password: 'sihZ1l4k8B',
+      database: 'sql11180430',
+      dateStrings: true,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 
 };
